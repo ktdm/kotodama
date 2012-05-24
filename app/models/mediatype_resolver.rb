a4eddb2043997197a1321174df4dcfd028a2cd8d
Aidan Hamwood <aidan.hamwood@gmail.com>
2012-05-24 17:13:22 +1000

    Progress implementing view formats & included mediatypes

--- app/models/mediatype_resolver.rb
+++ app/models/mediatype_resolver.rb
@@ -1,17 +1,22 @@
 class MediatypeResolver < ActionView::PathResolver
 
-  #Views are titled by :format - eg "html.erb"? "_"? is there an 'embedded html'/'document fragment' filetype?
-  # - what about partial images? filetype subdomains? need a general marking system
-  #    html.part? and html.blog?
+  #views["x"] is for converting media to x format
+  #views["x:y"] is an included file of mediatype x - may be for internal use only
 
   include Url
 
+  def find_templates(name, prefix, partial, details)
+    mediatype, name = name.index(":").nil? ? ["", name] : name.split(":")
+    path = Path.build(name, prefix, mediatype.downcase == "partial")
+    query(path, details, details[:formats])
+  end
+
   def query(path, details, formats)
     mediatype = Media.find(decode (path.prefix.empty? ? path.name : path.prefix)).mediatype
-    mediatype.data.views.select {|x| x.keys[0].downcase == mediatype.title.downcase }.map { |hash|
+    mediatype.data.views.reject {|x| formats.index(x.keys[0].downcase.to_sym).nil? }.map { |hash|
       ActionView::Template.new(hash.values[0], path, ActionView::Template::Handlers::ERB,
         :virtual_path => path.virtual,
-        :format       => :html, # ???
+        :format       => formats.first, #???
         :updated_at   => mediatype.updated_at) #<- for each template?
     }
   end
